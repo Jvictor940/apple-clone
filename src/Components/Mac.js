582ee8f425551e1b8c3d3365b91a796b50961e32
Jerry <Jvictor940@gmail.com>
2022-06-22 17:28:47 -0400

    Added video to Mac page

--- src/Components/Mac.js
+++ src/Components/Mac.js
@@ -5,7 +5,10 @@ function Mac() {
     console.log("yes it workks")
     return(
         <div className="Mac">
-            <h2 className="Mac1">Jerry Victor</h2>
+            {/* <h2 className="Mac1">Jerry Victor</h2> */}
+            <video className="background-video" autoPlay muted>
+                <source className="Video" src="../MacVid1medium_2x.mp4" type="video/mp4" controls></source>
+            </video>
         </div>
     )
 }
